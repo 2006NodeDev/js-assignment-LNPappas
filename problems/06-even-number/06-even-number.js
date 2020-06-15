55610722415af2532db5105448117158d2966d4d
LNPappas <LPappas@gmail.com>
2020-06-14 18:19:36 -0700

    iseven

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,28 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+/* 
+    use bitwise & operator:
+    number = 11:
+          1011 <- 11 in binary
+        & 0001 <- 1 in binary
+        --------
+          0001 <- 1 !== 0 and is odd
+
+    number = 14:
+          1110 <- 14 in binary
+        & 0001 <- 1 in binary
+        --------
+          0000 <- 0 === 0 and is even
+*/
+  return (someNum & 1) === 0;
 }
+
+// test cases:
+console.log(isEven(11));    // false
+console.log(isEven(14));    // true
+console.log(isEven(2));     // true
+console.log(isEven(1));     // false
+console.log(isEven(0));     // true
+console.log(isEven(-1));    // false
+console.log(isEven(-2));    // true
