bce0472f5c53573e37f183f247c616b963b9c3ec
LNPappas <LPappas@gmail.com>
2020-06-15 08:28:17 -0700

    object literal

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,17 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+// create object literal with params
+function Person(n, a){
+    // use this.variable to assign param to object variable
+    this.name = n;
+    this.age = a;    
+};
+
+// create instance of object literal and input params
+let boy = new Person("Harvey", 6);
+
+// use for loop to log all keys/attributes of object instance
+for(key in boy) console.log(`${key} : ${boy[key]}`);
+
